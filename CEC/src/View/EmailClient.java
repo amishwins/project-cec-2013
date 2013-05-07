72a4a62b183195fbcd030e96d77160c0bec12588
deyvid.william <deyvid.william@gmail.com>
2013-05-07 17:49:06 +0000

    First official C.E.C import - Starting from the scratch

---
+++ CEC/src/View/EmailClient.java
@@ -0,0 +1,16 @@
+/**
+ * @author Deyvid William / Created 07-May-13
+ */
+
+package View;
+
+import javax.swing.JFrame;
+
+public class EmailClient extends JFrame{
+    
+    public EmailClient(String title){
+        
+        super(title);
+    }   
+    
+}
