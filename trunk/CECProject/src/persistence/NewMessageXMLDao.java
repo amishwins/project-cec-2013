5f126bc43cf0c3d3171215c6d803144f50e2379d
pankajkapania@gmail.com <pankajkapania@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-05-11 22:02:36 +0000

    replaced DAO with persistence package!

--- trunk/CECProject/src/persistence/NewMessageXMLDao.java
+++ trunk/CECProject/src/persistence/NewMessageXMLDao.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Persistence;
+package persistence;
 
 import java.io.File;
 import javax.xml.parsers.DocumentBuilder;
