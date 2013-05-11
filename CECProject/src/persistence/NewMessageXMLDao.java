c486b6140b3356c7ad4acda046e1c8a1c54ffe11
pankajkapania <pankajkapania@gmail.com>
2013-05-11 21:38:37 +0000

    replaced DAO with persistence package!

--- CECProject/src/persistence/NewMessageXMLDao.java
+++ CECProject/src/persistence/NewMessageXMLDao.java
@@ -2,8 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package persistence;
-
+package Persistence;
 
 import java.io.File;
 import javax.xml.parsers.DocumentBuilder;
@@ -14,80 +13,65 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
- 
+
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 /**
- *
+ * 
  * @author Pankaj Kapania
  */
-public class NewMessageXMLDao implements NewMessageDao{
-   
-    public void save (String to, String subject, String body, String location){
-   
-       try {
- 
-		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
- 
-		// root elements
-		Document doc = docBuilder.newDocument();
-		Element rootElement = doc.createElement("company");
-		doc.appendChild(rootElement);
- 
-		// staff elements
-		Element staff = doc.createElement("Staff");
-		rootElement.appendChild(staff);
- 
-		// set attribute to staff element
-		Attr attr = doc.createAttribute("id");
-		attr.setValue("1");
-		staff.setAttributeNode(attr);
- 
-		// shorten way
-		// staff.setAttribute("id", "1");
- 
-		// firstname elements
-		Element firstname = doc.createElement("firstname");
-		firstname.appendChild(doc.createTextNode("yong"));
-		staff.appendChild(firstname);
- 
-		// lastname elements
-		Element lastname = doc.createElement("lastname");
-		lastname.appendChild(doc.createTextNode("mook kim"));
-		staff.appendChild(lastname);
- 
-		// nickname elements
-		Element nickname = doc.createElement("nickname");
-		nickname.appendChild(doc.createTextNode("mkyong"));
-		staff.appendChild(nickname);
- 
-		// salary elements
-		Element salary = doc.createElement("salary");
-		salary.appendChild(doc.createTextNode("100000"));
-		staff.appendChild(salary);
- 
-		// write the content into xml file
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		DOMSource source = new DOMSource(doc);
-		StreamResult result = new StreamResult(new File("C:\\file.xml"));
- 
-		// Output to console for testing
-		// StreamResult result = new StreamResult(System.out);
- 
-		transformer.transform(source, result);
- 
-		System.out.println("File saved!");
- 
-	  } catch (ParserConfigurationException pce) {
-		pce.printStackTrace();
-	  } catch (TransformerException tfe) {
-		tfe.printStackTrace();
-	  }
-	   
-        
-    }
-  }
+public class NewMessageXMLDao implements NewMessageDao {
+
+	public void save(String to, String subject, String body, String location) {
+
+		try {
+
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+			// root elements
+			Document doc = docBuilder.newDocument();
+			Element eMail = doc.createElement("E-Mail");
+			doc.appendChild(eMail);
+
+			// to
+			Element toLabel = doc.createElement("To");
+			toLabel.appendChild(doc.createTextNode(to));
+			eMail.appendChild(toLabel);
+
+			// subject
+			Element subjectLabel = doc.createElement("Subject");
+			subjectLabel.appendChild(doc.createTextNode(subject));
+			eMail.appendChild(subjectLabel);
+
+			// body
+			Element bodyLabel = doc.createElement("Body");
+			bodyLabel.appendChild(doc.createTextNode(body));
+			eMail.appendChild(bodyLabel);
+
+			// write the content into xml file
+			TransformerFactory transformerFactory = TransformerFactory
+					.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(
+					"C:\\temp\\file.xml"));
+
+			// Output to console for testing
+			// StreamResult result = new StreamResult(System.out);
+
+			transformer.transform(source, result);
+
+			System.out.println("File saved!");
+
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} catch (TransformerException tfe) {
+			tfe.printStackTrace();
+		}
+
+	}
+}
