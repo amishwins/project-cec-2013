cbd8a5f4feb2496b2ee3c11ff6360d869e148f68
deyvid.william@gmail.com <deyvid.william@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-05-13 16:35:06 +0000

    UI Enhancements

--- trunk/CECProject/src/view/NewMessage2.java
+++ trunk/CECProject/src/view/NewMessage2.java
@@ -1,20 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+ * NewMessage screen
+  */
+
 package view;
 
 import java.awt.BorderLayout;
-
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
-
 import javax.swing.*;
+import service.NewMessageService;
+
 
 public class NewMessage2 extends JFrame{
     
@@ -22,7 +23,7 @@ public class NewMessage2 extends JFrame{
   
         //Layout Manager
         super("New Message");
-        setSize(610, 410);
+        setSize(610, 420);
         setLayout(new BorderLayout());
         setResizable(false);
         setVisible(true);  
@@ -36,13 +37,32 @@ public class NewMessage2 extends JFrame{
         
         
         //Swing Components - Top Panel 
-        ImageIcon sendIcon = new ImageIcon("images/emailblue.png");
-        ImageIcon draftIcon = new ImageIcon("images/emailyellow.png");
-        JButton send = new JButton(" Send >> ",sendIcon );
-        JButton draft = new JButton("Save as Draft",draftIcon);
+        ImageIcon sendIcon = new ImageIcon("images/email_send.png");
+        ImageIcon draftIcon = new ImageIcon("images/email_draft.png");
+        ImageIcon discardIcon = new ImageIcon("images/email_discard.png");
+        JButton send = new JButton(" Send >>  ",sendIcon );
+        JButton draft = new JButton("Save as Draft ",draftIcon);
+        JButton discard = new JButton("Discard ",discardIcon);
         JToolBar bar = new JToolBar();  
+        bar.setFloatable(false);
         bar.add(send);
         bar.add(draft);
+        bar.add(discard);
+        
+        
+        send.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sendNewMessage(evt);}});
+        
+        draft.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                draftNewMessage(evt);}});
+        
+        discard.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                discardNewMessage(evt);}});
+        
+
 
         
         //Swing Components - Middle Panel
@@ -53,6 +73,9 @@ public class NewMessage2 extends JFrame{
         JLabel subject = new JLabel("Subject:");
         JTextField subjectField = new JTextField(22);
         
+        
+        
+                
         JPanel mid = new JPanel();  
         mid.setPreferredSize(new Dimension(320,300));     
         mid.add(to);
@@ -74,6 +97,35 @@ public class NewMessage2 extends JFrame{
         add(bar, BorderLayout.NORTH);    
         add(mid, BorderLayout.LINE_START);  
         add(scroll, BorderLayout.SOUTH);   
+                
         
       }    
+      
+      //Actions - Discard
+      private void discardNewMessage(ActionEvent evt) {                                
+        this.dispose();        
+    }                                        
+      
+      //Actions - Discard
+      private void draftNewMessage(ActionEvent evt) {   
+       
+       /* String to = toField.getText();
+        String cc = ccField.getText();
+        String subject = jTextField3.getText();
+        String body = jTextArea1.getText();
+        newEmailService.draftEmail(to, cc, subject, body);  */
+    }                                        
+      
+      //Actions - Discard
+      private void sendNewMessage(ActionEvent evt) {                                
+      /*  String to = jTextField1.getText();
+        String cc = jTextField2.getText();
+        String subject = jTextField3.getText();
+        String body = jTextArea1.getText();
+        newEmailService.sendEmail(to, cc, subject, body);   */    
+    }                                        
+            
 }
+
+
+
