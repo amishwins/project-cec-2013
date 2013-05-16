378b12ea789bb9750f9024386f3d99c1806835ad
amish.gala@gmail.com <amish.gala@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-05-16 18:37:45 +0000

    Moved this project to a Java Application which is runnable within Eclipse

--- trunk/CECProject/src/model/EmailImpl.java
+++ trunk/CECProject/src/model/EmailImpl.java
@@ -111,6 +111,8 @@ public class EmailImpl implements Email, Comparable<Email>{
 			
 			e.printStackTrace();
 		}
+		
+		// this is the latest code
 
 		return currentEmailDate.compareTo(anotherEmailDate);
 	}
