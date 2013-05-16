89edb2dab817788c8f84712d17f339f6ad81fadf
romeo.honvo <romeo.honvo@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-05-16 03:31:23 +0000

    Right CLick On Tree Almost Done

--- trunk/CECProject/src/view/FoldMgmt.java
+++ trunk/CECProject/src/view/FoldMgmt.java
@@ -1,5 +1,9 @@
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.io.File;
@@ -14,19 +18,20 @@ import javax.swing.event.TreeSelectionListener;
 		static String selectedFolder;
 		public static void createJTreeFrame(final String parent,final String root)
 		{
-                    
+			
+			
                     JFrame theTreeMgt = new JFrame();
                     theTreeMgt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                     theTreeMgt.setLayout(new GridLayout(1,1));
                     //JTree tree = new JTree();
-                    JTree tree = new JTree(addTree(root));	
+                    final JTree tree = new JTree(addTree(root));	
 
                     tree.addTreeSelectionListener(
                                     new TreeSelectionListener() {
                             public void valueChanged(TreeSelectionEvent select) {
 
                                     TreePath fullpath = select.getPath();
-                                    System.out.println("you selected");
+                                   // System.out.println("you selected");
                                     selectedFolder="";
                                     
                                     for (int i=0; i<fullpath.getPathCount()-1; i++)
@@ -35,57 +40,52 @@ import javax.swing.event.TreeSelectionListener;
                                     }
                                     selectedFolder+=fullpath.getLastPathComponent().toString();
                                     selectedFolder=parent+selectedFolder;
+                                    System.out.println("you selected----   "+selectedFolder);
                             }
                     });			
                     tree.setSize(100, 400);//.setSize(200.200);
                     tree.setVisible(true);			
                     JScrollPane scrollpane = new JScrollPane(tree);
 
-
-
-
-
-
-
                     JButton Bcreate = new JButton("CREATE");
                     Bcreate.addActionListener
-                    ( 
-                        new ActionListener() 
-                        {				
-                            public void actionPerformed(ActionEvent click)
-                            {
-                                if(selectedFolder!=null)
+                    (new ActionListener(){public void actionPerformed(ActionEvent click) {create_click();}});
+                        			                           
+                    JButton Bdelete = new JButton("DELETTE");
+                    Bdelete.addActionListener
+                    (new ActionListener(){public void actionPerformed(ActionEvent click) {delete_click();}});
+
+                    JPanel Buttons = new JPanel();
+                    Buttons.add(Bcreate,BorderLayout.WEST);Buttons.add(Bdelete,BorderLayout.EAST);           				
+
+                                    
+
+                    tree.addKeyListener(KenterL);
+
+
+
+                    final JPopupMenu Pmenu=new JPopupMenu();      
+                    JMenuItem menuItem;
+                    menuItem = new JMenuItem("Delete Ctrl+D");
+                    Pmenu.add(menuItem);
+                    menuItem = new JMenuItem("Create Ctrl+C");
+                    Pmenu.add(menuItem);						
+
+                    menuItem.addActionListener(new ActionListener()
+                        {
+                           public void actionPerformed(ActionEvent e){}
+                           });
+                                tree.addMouseListener(new MouseAdapter()
                                 {
-                                        String folderName = JOptionPane.showInputDialog(null, "Folder Name","",  1);
-                                        btnCreate(selectedFolder,folderName);		        	
-                                }
-                                else
-                                        JOptionPane.showMessageDialog(null, "SELECT LOCATION");
-                            }
+                                   public void mouseReleased(MouseEvent Me)
+                                   {
+                                        int selFold = tree.getRowForLocation(Me.getX(), Me.getY());
+                                        TreePath selection = tree.getPathForLocation(Me.getX(), Me.getY());
+                                        if(selFold != -1)                                     
+                                             Pmenu.show(Me.getComponent(), Me.getX(), Me.getY());	
+                                   }
                         }
                     );
-
-			
-                JButton Bdelete = new JButton("DELETTE");
-                Bdelete.addActionListener
-                ( 
-                                new ActionListener()
-                                {						
-                                    public void actionPerformed(ActionEvent click)
-                                    {        	                                       
-                                        if(selectedFolder!=null)			        		
-                                                btnDelete(selectedFolder);
-                                        else
-                                                JOptionPane.showMessageDialog(null, "SELECT FOLDER TO DELETE");				        	
-                                    }
-                                }
-                );
-
-
-
-                JPanel Buttons = new JPanel();
-                Buttons.add(Bcreate,BorderLayout.WEST);Buttons.add(Bdelete,BorderLayout.EAST);
-
 
                 theTreeMgt.add(scrollpane);		
                 theTreeMgt.add(Buttons);
@@ -94,11 +94,56 @@ import javax.swing.event.TreeSelectionListener;
                 theTreeMgt.setVisible(true);		
 
         }	
-        public static String Selection()
-        {			
-                String selection="SELECTION";
-                return selection;
-        }
+		
+		
+		
+		static KeyListener KenterL = new KeyListener()
+		{
+			
+			//Perform action Cut when you press ctrl+X keys to cut the selected cell
+			public void keyPressed(KeyEvent ke) 
+			{
+				
+				if(ke.isControlDown())
+				{	
+					if(ke.getKeyCode()==ke.VK_D)
+					{ 
+						delete_click();
+					}
+					
+					else if (ke.getKeyCode()==ke.VK_C)
+					{						
+						create_click();
+					}					
+					
+				}
+				
+				if(ke.getKeyCode()==ke.VK_ESCAPE)
+				{
+					// JOptionPane.showMessageDialog(null, "ESCAPE");
+				}
+				
+			}	
+			
+			
+			@Override
+			public void keyReleased(KeyEvent ke) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyTyped(KeyEvent ke) {
+				// TODO Auto-generated method stub
+				
+			}				
+		};
+
+		
+		
+        
+        
+        
             public static DefaultMutableTreeNode addTree(String dirname) 
             {
                     File file = new File(dirname);
@@ -115,7 +160,31 @@ import javax.swing.event.TreeSelectionListener;
                     }
                     return (root);
             }
-
+            
+            
+            
+            
+            
+            
+            
+            
+            public static void create_click()
+            {
+        		if(selectedFolder!=null)
+                {
+                        String folderName = JOptionPane.showInputDialog(null, "Folder Name","",  1);
+                        btnCreate(selectedFolder,folderName);		        	
+                }
+                else
+                        JOptionPane.showMessageDialog(null, "SELECT LOCATION");
+        	}
+            public static void delete_click()
+            {
+            if(selectedFolder!=null)			        		
+                    btnDelete(selectedFolder);
+            else
+                    JOptionPane.showMessageDialog(null, "SELECT FOLDER TO DELETE");	
+            }
             public static void btnDelete(String folderName)//,String folderName) 
             {
 
@@ -133,16 +202,17 @@ import javax.swing.event.TreeSelectionListener;
                    else           		    	             
                       direct.mkdir();		    	  
                    
-            }					
-	public static void main(String[] args) 
-	{
+            }	
+            
+            public static void main(String[] args) 
+            {
 		//JFrame theTree = new JFrame();
 		
 		
 		System.out.println("Start");
 		//final String pat = "emails";//"G:\\SUMMER2013";
-		String par ="";//G:\\";
-		String pat ="emails";//"G:\\SUMMER2013";//"emails";
+		String par ="G:\\";//"";//
+		String pat ="G:\\SUMMER2013";//"emails";
 		createJTreeFrame(par,pat);                
 		System.out.println("End");
 
