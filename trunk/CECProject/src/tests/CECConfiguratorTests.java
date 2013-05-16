e3e6d5de3517b20f2bf7570d9522f03bfe46ae89
amish.gala@gmail.com <amish.gala@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-05-16 23:21:56 +0000

    Add the configurator and think about integration tests

---
+++ trunk/CECProject/src/tests/CECConfiguratorTests.java
@@ -0,0 +1,44 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import cec.config.CECConfigurator;
+
+public class CECConfiguratorTests {
+	
+	private CECConfigurator cut;
+
+	@Before
+	public void setUp() throws Exception {
+		cut = new CECConfigurator();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void addedValueCanBeRetrieved() {
+		cut.put("Dog", "Bark");
+		assertTrue(cut.get("Dog").equals("Bark"));
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void valueNotFoundThrowsException() {
+		assertFalse(cut.get("Dog").equals("Meow"));
+	}
+	
+	@Test
+	public void getDefaultValues() {
+		assertTrue(cut.get("Inbox").equals("emails/Inbox"));
+		assertTrue(cut.get("Drafts").equals("emails/Drafts"));
+		assertTrue(cut.get("Sent").equals("emails/Sent"));
+	}
+	
+	
+
+}
---
+++ trunk/CECProject/src/tests/integration/EmailDaoIntegrationTests.java
@@ -0,0 +1,27 @@
+package tests.integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+// (maybe move this comment) more dangerous because it creates objects and deletes objects in the file system
+public class EmailDaoIntegrationTests {
+	
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
