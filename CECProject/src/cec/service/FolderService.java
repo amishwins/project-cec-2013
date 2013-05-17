17df591fcc5a6c51eb515f2b4ff5f70b102c8bff
pankajkapania <pankajkapania@gmail.com>
2013-05-17 18:31:27 +0000

    Removed main method from service.

--- CECProject/src/cec/service/FolderService.java
+++ CECProject/src/cec/service/FolderService.java
@@ -12,7 +12,7 @@ public class FolderService {
 	
 	static Folder folder;
 	
-	public static Iterable<EmailViewEntity> loadEmail(String folderPath){
+	public Iterable<EmailViewEntity> loadEmails(String folderPath){
 		List<EmailViewEntity> emailListInView = new ArrayList<EmailViewEntity>();
 		folder = FolderFactory.getFolder(folderPath);
 		Iterable<Email> emailListInModel = folder.loadEmails();
@@ -33,23 +33,6 @@ public class FolderService {
 		return emailListInView;
 	}
 	
-	public static void main(String args[]){
-		
-		Iterable<EmailViewEntity> emailsListInView = loadEmail("emails/Drafts");
-		for(EmailViewEntity emailInView :emailsListInView ){
-			System.out.println("Emails In Folder");
-			System.out.println(emailInView.getId());
-			System.out.println(emailInView.getFrom());
-			System.out.println(emailInView.getTo());
-			System.out.println(emailInView.getCC());
-			System.out.println(emailInView.getSubject());
-			System.out.println(emailInView.getBody());
-			System.out.println(emailInView.getLastModifiedTime());
-			System.out.println(emailInView.getSentTime());
-			System.out.println(emailInView.getFolder());
-			
-		}
-		
-	}
+	
 
 }
