393f53dc82daea3a4354c758e359b33a907d2938
pankajkapania@gmail.com <pankajkapania@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-05-20 19:38:13 +0000

    Added FolderAlreadyExistsException class to implement  functionality to prevent user from creating duplicate folders.

--- trunk/CECProject/src/tests/integration/FolderServiceModelAndDaoIntegrationTests.java
+++ trunk/CECProject/src/tests/integration/FolderServiceModelAndDaoIntegrationTests.java
@@ -8,11 +8,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import cec.service.FolderService;
+import exceptions.FolderAlreadyExistsException;
 
 public class FolderServiceModelAndDaoIntegrationTests {
+	String rootFolder = "emails";
+	String inboxFolder = "Inbox";
 	String systemFolderName = "emails/Inbox";
-	String userFolderLevel1 = "integrationtests";
-	String completePath = systemFolderName+"/"+userFolderLevel1;
+	String userFolder = "integrationtests";
+	String completePath = systemFolderName+"/"+userFolder;
 	FolderService folderService;
 	
 	@Before
@@ -22,15 +25,31 @@ public class FolderServiceModelAndDaoIntegrationTests {
 
 	@After
 	public void tearDown() throws Exception {
+		folderService.delete(completePath);
 	}
 
 	@Test
 	public void createAndDeleteFolder() {
-		folderService.createSubFolder(systemFolderName,userFolderLevel1);
+		folderService.delete(completePath);
+		folderService.createSubFolder(systemFolderName,userFolder);
 		List<String> folder = (List<String>)folderService.loadHierarchy();
 		assertTrue(folder.contains(completePath));
 		folderService.delete(completePath);
 	}
+	
+	@Test (expected=FolderAlreadyExistsException.class)
+	public void ShouldThrowWhenFolderAlreadyExists() {
+		folderService.delete(completePath);
+		folderService.createSubFolder(systemFolderName,userFolder);
+		folderService.createSubFolder(systemFolderName,userFolder);
+		folderService.delete(completePath);
+	}
+	
+	@Test (expected=FolderAlreadyExistsException.class)
+	public void ShouldThrowWhenUserTriesToCreateSystemFolder() {
+		folderService.createSubFolder(rootFolder,inboxFolder);		
+	}
+	
 	
 
 }
