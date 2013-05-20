f98c3cf00a0dd3553cdaa34047af41d3a7bc6618
amish.gala@gmail.com <amish.gala@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-05-20 17:31:59 +0000

    Rename file to be consistent

--- trunk/CECProject/src/tests/integration/IntegrationTests.java
+++ trunk/CECProject/src/tests/integration/IntegrationTests.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ 
 	EmailModelAndDaoIntegrationTests.class,
-	EmailServiceModelAndDaoIntegrationTest.class,
+	EmailServiceModelAndDaoIntegrationTests.class,
 	FolderServiceModelAndDaoIntegrationTests.class })
 
 public class IntegrationTests {
