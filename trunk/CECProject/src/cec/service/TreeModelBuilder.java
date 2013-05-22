3597d09c95cc41aaf08e23aa751fbb27c72ea592
pankajkapania@gmail.com <pankajkapania@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-05-22 03:21:34 +0000

    Refactored TreeModelBuilder Class

--- trunk/CECProject/src/cec/service/TreeModelBuilder.java
+++ trunk/CECProject/src/cec/service/TreeModelBuilder.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cec.service;
 
 import java.util.Enumeration;
@@ -9,76 +5,67 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 
 /**
- *
+ * 
  * @author a_gala
  */
 public class TreeModelBuilder {
-    private DefaultTreeModel model;
+	private DefaultTreeModel cecModel;
 
-    public TreeModelBuilder(DefaultMutableTreeNode root) {
-        model = new DefaultTreeModel(root);
-    }
-    
-    public DefaultTreeModel buildTreeNodeFromFileList(Iterable<String> hierarchy) {
-        
-        for(String folder: hierarchy) {
-            buildTreeFromFolders(folder);
-        }
-        return model;
-    }
-    
-    private void buildTreeFromFolders(final String str) {
-        // Fetch the root node
-        DefaultMutableTreeNode root = (DefaultMutableTreeNode) model.getRoot();
+	public TreeModelBuilder(DefaultMutableTreeNode root) {
+		cecModel = new DefaultTreeModel(root);
+	}
 
-        // Split the string around the delimiter
-        String [] strings = str.split("/");
+	public DefaultTreeModel buildTreeNodeFromFileList(Iterable<String> hierarchy) {
 
-        // Create a node object to use for traversing down the tree as it 
-        // is being created
-        DefaultMutableTreeNode node = root;
+		for (String folder : hierarchy) {
+			buildTreeFromFolders(folder);
+		}
+		return cecModel;
+	}
 
-        // Iterate of the string array
-        for (String s: strings) {
-            // Look for the index of a node at the current level that
-            // has a value equal to the current string
-            int index = childIndex(node, s);
+	private void buildTreeFromFolders(final String relativeFolderPath) {
 
-            // Index less than 0, this is a new node not currently present on the tree
-            if (index < 0) {
-                // Add the new node
-                DefaultMutableTreeNode newChild = new DefaultMutableTreeNode(s);
-                node.insert(newChild, node.getChildCount());
-                node = newChild;
-            }
-            // Else, existing node, skip to the next string
-            else {
-                node = (DefaultMutableTreeNode) node.getChildAt(index);
-            }
-        }
-    }
+		DefaultMutableTreeNode root = (DefaultMutableTreeNode) cecModel
+				.getRoot();
 
-    /**
-     * Returns the index of a child of a given node, provided its string value.
-     * 
-     * @param node The node to search its children
-     * @param childValue The value of the child to compare with
-     * @return The index
-     */
-    private int childIndex(final DefaultMutableTreeNode node, final String childValue) {
-        @SuppressWarnings("unchecked")
-		Enumeration<DefaultMutableTreeNode> children = node.children();
-        DefaultMutableTreeNode child = null;
-        int index = -1;
+		String[] folders = relativeFolderPath.split("/");
 
-        while (children.hasMoreElements() && index < 0) {
-            child = children.nextElement();
+		DefaultMutableTreeNode node = root;
 
-            if (child.getUserObject() != null && childValue.equals(child.getUserObject())) {
-                index = node.getIndex(child);
-            }
-        }
+		for (String folder : folders) {
 
-        return index;
-    }
+			int index = subFolderIndex(node, folder);
+
+			if (index < 0) {
+
+				DefaultMutableTreeNode subFolder = new DefaultMutableTreeNode(
+						folder);
+				node.insert(subFolder, node.getChildCount());
+				node = subFolder;
+			}
+
+			else {
+				node = (DefaultMutableTreeNode) node.getChildAt(index);
+			}
+		}
+	}
+
+	private int subFolderIndex(final DefaultMutableTreeNode parentFolder,
+			final String subFolderPath) {
+		Enumeration<DefaultMutableTreeNode> subFolders = parentFolder
+				.children();
+		DefaultMutableTreeNode subFolder = null;
+		int index = -1;
+
+		while (subFolders.hasMoreElements() && index < 0) {
+			subFolder = subFolders.nextElement();
+
+			if (subFolder.getUserObject() != null
+					&& subFolderPath.equals(subFolder.getUserObject())) {
+				index = parentFolder.getIndex(subFolder);
+			}
+		}
+
+		return index;
+	}
 }
