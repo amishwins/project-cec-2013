6ea19aee183dc8bd106565ce9ccec073ebde6b6e
amish.gala <amish.gala@gmail.com>
2013-05-23 00:26:10 +0000

    JavaDocs for many model classes

--- CECProject/src/cec/model/Folder.java
+++ CECProject/src/cec/model/Folder.java
@@ -15,6 +15,10 @@ import cec.persistence.FolderDao;
 import cec.persistence.FolderDaoFactory;
 import exceptions.RootFolderSubfolderCreationException;
 
+/**
+ * Folder model object. A runtime folder object represents a physical folder from the file system
+ *
+ */
 public abstract class Folder {
 	
 	private String path;
@@ -26,6 +30,9 @@ public abstract class Folder {
         setFolderDao(FolderDaoFactory.getFolderDaoInstance());
     }
 
+	/**
+	 * @return the path to the file object relative to the system emails folder
+	 */
 	public String getPath() {
 		return path;
 	}
@@ -34,8 +41,16 @@ public abstract class Folder {
     	this.folderDao = folderDao;    	
     }
     
+    /**
+     * Abstract delete method to be implemented by the children of this class
+     */
     public abstract void delete();
     
+    /**
+     * Create a subfolder in the file system underneath this folder 
+     * 
+     * @param newFolderName the foldername which is to be created under the current folder
+     */
     public void createSubFolder(String newFolderName){
     	if (getPath().equals(CECConfigurator.getReference().get("emails"))) {
     		throw new RootFolderSubfolderCreationException();
@@ -43,6 +58,9 @@ public abstract class Folder {
     	folderDao.createSubFolder(getPath(), newFolderName);
     }
 	
+	/**
+	 * @return the collection of emails which are currently in this folder
+	 */
 	public Iterable<Email> loadEmails() {
 		emailsInFolder = new LinkedList<Email>();
 		Iterable<Map<String,String>> emailsData = folderDao.loadEmails(path);
--- CECProject/src/cec/model/FolderFactory.java
+++ CECProject/src/cec/model/FolderFactory.java
@@ -2,8 +2,19 @@ package cec.model;
 
 import cec.config.CECConfigurator;
 
+/**
+ * FolderFactory class provides a static factory method to build Folder objects.
+ * This class checks with the system configurator if the folder to be created 
+ * is a System Folder or a User Folder, and then return the correct type.
+ *
+ */
 public class FolderFactory {
 	
+	/**
+	 * @param path the string path to the folder on the file system for which 
+	 * a Folder object is to be returned
+	 * @return Folder either a SystemFolder or a UserFolder
+	 */
 	public static Folder getFolder(String path) {
 		// What do we do if path is empty? invalid argument?		
 		if (CECConfigurator.getReference().isPathForASystemFolder(path)) {
