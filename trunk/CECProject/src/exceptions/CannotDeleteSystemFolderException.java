bd72955e3190e52c62787f6945e450bc65b625d0
pankajkapania@gmail.com <pankajkapania@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-01 09:08:05 +0000

    Added new exceptions for meeting objects.

--- trunk/CECProject/src/exceptions/CannotDeleteSystemFolderException.java
+++ trunk/CECProject/src/exceptions/CannotDeleteSystemFolderException.java
@@ -4,7 +4,7 @@ public class CannotDeleteSystemFolderException extends RuntimeException {
 	private static final long serialVersionUID = 6196097603607327382L;
 	
 	public CannotDeleteSystemFolderException (){
-		super("CannotDeleteSystemFolderException");
+		super("Cannot Delete System Folder!");
 	}
 	
     public CannotDeleteSystemFolderException (String messege){
--- trunk/CECProject/src/exceptions/RootFolderSubfolderCreationException.java
+++ trunk/CECProject/src/exceptions/RootFolderSubfolderCreationException.java
@@ -5,7 +5,7 @@ public class RootFolderSubfolderCreationException extends RuntimeException {
 	private static final long serialVersionUID = -529431082654759631L;
 
 	public RootFolderSubfolderCreationException (){
-		super("CannotDeleteSystemFolderException");
+		super("Cannot create subfolders under root folders(emails or meetings)!");
 	}
 	
     public RootFolderSubfolderCreationException (String messege){
