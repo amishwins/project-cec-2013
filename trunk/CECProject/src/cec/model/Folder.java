caab6933d2b6bfaa0c24b03640ae1d4667b5e102
pankajkapania@gmail.com <pankajkapania@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-01 08:58:33 +0000

    Made changes to accommodate MeetingFolder in the hierarchy.

--- trunk/CECProject/src/cec/model/Folder.java
+++ trunk/CECProject/src/cec/model/Folder.java
@@ -4,16 +4,8 @@
  */
 package cec.model;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
-import cec.config.CECConfigurator;
 import cec.persistence.FolderDao;
 import cec.persistence.FolderDaoFactory;
-import exceptions.RootFolderSubfolderCreationException;
 
 /**
  * Folder model object. A runtime folder object represents a physical folder from the file system
@@ -22,7 +14,6 @@ import exceptions.RootFolderSubfolderCreationException;
 public abstract class Folder {
 	
 	private String path;
-	private List<Email> emailsInFolder;
 	protected FolderDao folderDao;
 	
     public Folder(String path) {
@@ -51,36 +42,12 @@ public abstract class Folder {
      * 
      * @param newFolderName the foldername which is to be created under the current folder
      */
-    public void createSubFolder(String newFolderName){
-    	if (getPath().equals(CECConfigurator.getReference().get("emails"))) {
-    		throw new RootFolderSubfolderCreationException();
-    	}
-    	folderDao.createSubFolder(getPath(), newFolderName);
-    }
+    public abstract void createSubFolder(String newFolderName);
 	
 	/**
 	 * @return the collection of emails which are currently in this folder
 	 */
-	public Iterable<Email> loadEmails() {
-		emailsInFolder = new LinkedList<Email>();
-		Iterable<Map<String,String>> emailsData = folderDao.loadEmails(path);
-		
-		for(Map<String,String> emailData: emailsData) {
-			EmailBuilder emailBuilder = new EmailBuilder();
-			Email email = emailBuilder.withId(UUID.fromString(emailData.get("Id")))
-				.withFrom(emailData.get("From"))
-				.withTo(emailData.get("To"))
-				.withCC(emailData.get("CC"))
-				.withSubject(emailData.get("Subject"))
-				.withBody(emailData.get("Body"))
-				.withLastModifiedTime(emailData.get("LastModifiedTime"))
-				.withSentTime(emailData.get("SentTime"))
-				.withParentFolder(FolderFactory.getFolder(emailData.get("ParentFolder")))
-				.build();
-			emailsInFolder.add(email);
-		}	
-		Collections.sort(emailsInFolder);
-		return emailsInFolder;
-	}
+	public abstract Iterable<Email> loadEmails();
 	
+	public abstract Iterable<Meeting> loadMeetings();
 }
