694a2a2e3fdbaceeb05f0e71aa0b79af3297e2d6
pankajkapania@gmail.com <pankajkapania@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-01 09:02:10 +0000

    Removed unnecessary import.

--- trunk/CECProject/src/cec/model/MeetingsFolder.java
+++ trunk/CECProject/src/cec/model/MeetingsFolder.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import cec.persistence.FolderDao;
 import exceptions.CannotDeleteSystemFolderException;
 import exceptions.RootFolderSubfolderCreationException;
 
