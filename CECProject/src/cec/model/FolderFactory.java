f7a1502cada15b5db1db6a639513c4ff49fba00e
pankajkapania <pankajkapania@gmail.com>
2013-06-01 18:09:16 +0000

    Changed Classes names to more appropriate.

--- CECProject/src/cec/model/FolderFactory.java
+++ CECProject/src/cec/model/FolderFactory.java
@@ -19,11 +19,11 @@ public class FolderFactory {
 	public static Folder getFolder(String path) {
 		// What do we do if path is empty? invalid argument?
 		if (CECConfigurator.getReference().isPathForAEmailSystemFolder(path)) {
-			return new SystemFolder(path);
+			return new EmailsSystemFolder(path);
 		} else if (CECConfigurator.getReference()
 				.isPathForAMeetingSystemFolder(path)) {
 			return new MeetingsFolder(path);
 		} else
-			return new UserFolder(path);
+			return new EmailsUserFolder(path);
 	}
 }
