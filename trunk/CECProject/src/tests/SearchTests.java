93975a982bc73668a112578d83537bcd5c6d4bc6
romeo.honvo <romeo.honvo@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-01 20:43:00 +0000

    Search Test

---
+++ trunk/CECProject/src/tests/SearchTests.java
@@ -0,0 +1,35 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cec.model.Search;
+//import cec.model.Email;
+
+public class SearchTests {
+
+	Search s;
+	//Email email;
+	String emailString;
+	String toFind;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		//s = new Search(emailString,toFind);
+	}
+	
+	@Test
+	public void searchEntireEmail() {
+		String entireEmail = "the entire email";
+		//assertTrue(s.isEmailMatch());
+	}
+	
+	
+	
+	
+	
+	
+}
