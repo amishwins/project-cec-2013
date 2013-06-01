037c3be5b2928e274cab6522ed83d7298d91e723
deyvid.william <deyvid.william@gmail.com>
2013-06-01 21:16:42 +0000

    Search Feature - Junit Test

--- CECProject/src/tests/SearchTests.java
+++ CECProject/src/tests/SearchTests.java
@@ -6,30 +6,27 @@ import org.junit.Before;
 import org.junit.Test;
 
 import cec.model.Search;
-//import cec.model.Email;
 
 public class SearchTests {
 
-	Search s;
-	//Email email;
-	String emailString;
-	String toFind;
+	String source;
+	String pattern;	
 	
-	
-	@Before
-	public void setUp() throws Exception {
-		//s = new Search(emailString,toFind);
+	@Test
+	public void searchContentWithValidText() {
+		this.source = "Email Content";
+		this.pattern = "Email";
+		Search searchObj = new Search(source, pattern);
+		assertTrue(searchObj.isMatch());
 	}
 	
 	@Test
-	public void searchEntireEmail() {
-		String entireEmail = "the entire email";
-		//assertTrue(s.isEmailMatch());
+	public void searchContentWithCaseDiffs() {
+		this.source = "Email Content";
+		this.pattern = "eMaIl";
+		Search searchObj = new Search(source, pattern);
+		assertTrue(searchObj.isMatch());
 	}
 	
-	
-	
-	
-	
-	
+	 	
 }
