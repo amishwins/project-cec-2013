bb3990e3d81a20a9fa64755724d6f95decdd485d
pankajkapania@gmail.com <pankajkapania@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-01 08:59:57 +0000

    Added MeetingFolder as a new System Folder to contain meeting objects.

---
+++ trunk/CECProject/src/cec/model/MeetingsFolder.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cec.model;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import cec.persistence.FolderDao;
+import exceptions.CannotDeleteSystemFolderException;
+import exceptions.RootFolderSubfolderCreationException;
+
+/**
+ * Folder model object. A runtime folder object represents a physical folder
+ * from the file system
+ * 
+ */
+public class MeetingsFolder extends Folder {
+
+	public MeetingsFolder(String path) {
+		super(path);
+    }
+
+	/**
+	 * @return the collection of emails which are currently in this folder
+	 */
+	@Override
+	public Iterable<Meeting> loadMeetings() {
+		List<Meeting> meetingsInFolder = new LinkedList<Meeting>();
+		Iterable<Map<String, String>> meetingsData = folderDao.loadMeetings(getPath());
+
+		for (Map<String, String> meetingData : meetingsData) {
+			MeetingBuilder meetingBuilder = new MeetingBuilder();
+			Meeting meeting = meetingBuilder
+					.withId(UUID.fromString(meetingData.get("Id")))
+					.withFrom(meetingData.get("From"))
+					.withAttendees(meetingData.get("Attendees"))
+					.withStartDate(meetingData.get("MeetingStartDate"))
+					.withEndDate(meetingData.get("MeetingEndDate"))
+					.withStartTime(meetingData.get("MeetingStartTime"))
+					.withEndTime(meetingData.get("MeetingEndTime"))
+					.withPlace(meetingData.get("MeetingPlace"))
+					.withSubject(meetingData.get("Subject"))
+					.withBody(meetingData.get("Body"))
+					.withLastModifiedTime(meetingData.get("LastModifiedTime"))
+					.withSentTime(meetingData.get("SentTime"))
+					.withParentFolder(
+							FolderFactory.getFolder(meetingData
+									.get("ParentFolder"))).build();
+			meetingsInFolder.add(meeting);
+		}
+		Collections.sort(meetingsInFolder);
+		return meetingsInFolder;
+	}
+
+	@Override
+	public Iterable<Email> loadEmails() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void createSubFolder(String newFolderName) {
+		throw new RootFolderSubfolderCreationException();
+	}
+
+	@Override
+	public void delete() {
+		handleSystemDelete();
+	}
+
+	protected void handleSystemDelete() {
+		throw new CannotDeleteSystemFolderException();
+	}
+	
+	
+}
