bb8391d13be3cd603f005b71c2dcccd5cb8e3071
pankajkapania@gmail.com <pankajkapania@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-01 18:11:56 +0000

    Removed Unnecessary classes as we optimized  the hierarchy.

--- trunk/CECProject/src/cec/persistence/EmailTemplateDao.java
+++ trunk/CECProject/src/cec/persistence/EmailTemplateDao.java
@@ -1,9 +1,6 @@
 package cec.persistence;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Map;
-import java.util.UUID;
 
 /**
  * The Interface EmailTemplateDao exposes the lower level methods to
