d3325d6650d319426b7fc5ce39ea388c2f71c71f
pankajkapania <pankajkapania@gmail.com>
2013-06-01 08:58:46 +0000

    Made changes to accommodate MeetingFolder in the hierarchy.

--- CECProject/src/cec/model/FolderFactory.java
+++ CECProject/src/cec/model/FolderFactory.java
@@ -4,23 +4,26 @@ import cec.config.CECConfigurator;
 
 /**
  * FolderFactory class provides a static factory method to build Folder objects.
- * This class checks with the system configurator if the folder to be created 
- * is a System Folder or a User Folder, and then return the correct type.
- *
+ * This class checks with the system configurator if the folder to be created is
+ * a System Folder or a User Folder, and then return the correct type.
+ * 
  */
 public class FolderFactory {
-	
+
 	/**
-	 * @param path the string path to the folder on the file system for which 
-	 * a Folder object is to be returned
+	 * @param path
+	 *            the string path to the folder on the file system for which a
+	 *            Folder object is to be returned
 	 * @return Folder either a SystemFolder or a UserFolder
 	 */
 	public static Folder getFolder(String path) {
-		// What do we do if path is empty? invalid argument?		
-		if (CECConfigurator.getReference().isPathForASystemFolder(path)) {
+		// What do we do if path is empty? invalid argument?
+		if (CECConfigurator.getReference().isPathForAEmailSystemFolder(path)) {
 			return new SystemFolder(path);
-		}
-		else 
+		} else if (CECConfigurator.getReference()
+				.isPathForAMeetingSystemFolder(path)) {
+			return new MeetingsFolder(path);
+		} else
 			return new UserFolder(path);
 	}
 }
