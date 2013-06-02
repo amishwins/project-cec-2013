fdf7bc7c7ed941d3872f28844a5714f86ac7a72f
amish.gala <amish.gala@gmail.com>
2013-06-02 02:46:50 +0000

    Fooling around with the placeholder stuff. Not working so great!

--- CECProject/src/cec/view/EmailFrame.java
+++ CECProject/src/cec/view/EmailFrame.java
@@ -6,6 +6,8 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.util.List;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -28,25 +30,24 @@ import javax.swing.text.JTextComponent;
 
 import cec.config.CECConfigurator;
 import cec.service.EmailService;
+import cec.service.PlaceholderHelper;
 import cec.service.TemplateService;
-
 
 /**
  * Email Class extends JFRAME <br>
  * show a graphic windows allowing user to read or write an email.<br>
- * uses <code>JTextField</code> and <code>JTextArea</code> components to set editable fields<br>
+ * uses <code>JTextField</code> and <code>JTextArea</code> components to set
+ * editable fields<br>
  * uses <code>JButtons</code> and <code>JMenuItem </code>to provides options:<br>
  * <b>sendEmail()</b> <br>
  * <b>draftEmail()</b> <br>
  * Provides <br>
- * empty <code>JFrame</code> filled with existing<br> 
+ * empty <code>JFrame</code> filled with existing<br>
  * <code>email</code> for the parameterized constructor<br>
  * and empty <code>JFrame</code> for the default constructor<br>
  * used for new <code>email<code>
  * <p>
  */
-
-
 
 public class EmailFrame extends JFrame {
 	private static final long serialVersionUID = 6361797821203537189L;
@@ -61,14 +62,14 @@ public class EmailFrame extends JFrame {
 	JTextField ccField = new JTextField("", 65);
 	JTextField subjectField = new JTextField("", 65);
 	JTextArea bodyField = new JTextArea("", 15, 20);
-	
+
 	List<JTextComponent> componentsToValidate;
 
 	JButton reply = new JButton(" Reply ");
 	JButton forward = new JButton(" Forward ");
 	JButton draft = new JButton("Save as Draft ");
 	JButton send = new JButton(" Send >>   ");
-	
+
 	JButton saveTemplate = new JButton(" Save Template ");
 	JButton overwriteTemplate = new JButton(" Save Template ");
 
@@ -86,17 +87,20 @@ public class EmailFrame extends JFrame {
 	/**
 	 * Parameterized constructor of the Email Class.
 	 * <p>
-	 * Provides non Empty <code>JFrame</code> filled with existing<br> 
+	 * Provides non Empty <code>JFrame</code> filled with existing<br>
 	 * email that are editable only if it belongs to <br>
 	 * Draft folder.<br>
 	 * Available fields are Empty<br>
 	 * <code>JTextField</code> with label <b>"To:"</b><br>
 	 * <code>JTextField</code> with label <b>"Cc:"</b><br>
-	 * <code>JTextField</code> with label <b>"Subject:" </b> with a maximum of 250 Characters <br>
+	 * <code>JTextField</code> with label <b>"Subject:" </b> with a maximum of
+	 * 250 Characters <br>
 	 * <code>JTextArea</code> with label <b>"Body"</b>
 	 * <p>
-	 * @param  email  an <code>email</code> object that provides the <br>
-	 * 				  values used to set the JFrame
+	 * 
+	 * @param email
+	 *            an <code>email</code> object that provides the <br>
+	 *            values used to set the JFrame
 	 */
 	public EmailFrame(EmailViewEntity email) {
 		emailView = email;
@@ -110,7 +114,8 @@ public class EmailFrame extends JFrame {
 		switch (context) {
 		case APPLY:
 			// get the selected template, create an email normally
-			templateView = templateService.getTemplateEntity(EmailClient.getReference().getSelectedTemplateFromDialog().toString());
+			templateView = templateService.getTemplateEntity(EmailClient
+					.getReference().getSelectedTemplateFromDialog().toString());
 			emailView = new EmailViewEntity();
 			id = UUID.randomUUID();
 			setNewMessage();
@@ -124,31 +129,59 @@ public class EmailFrame extends JFrame {
 			initialize();
 			break;
 		case EDIT:
-			templateView = templateService.getTemplateEntity(EmailClient.getReference().getSelectedTemplateFromDialog().toString());
+			templateView = templateService.getTemplateEntity(EmailClient
+					.getReference().getSelectedTemplateFromDialog().toString());
 			setOverwriteTemplate();
 			initialize();
 			setTemplateFields();
 			break;
 		default:
-			throw new RuntimeException("Cannot handle this type of context");				
+			throw new RuntimeException("Cannot handle this type of context");
 		}
 	}
-	
+
 	private void createHandlerForSpecialFields() {
-		
-		
+
+		// extract the ${} placeholders for each one.
+		// add object and positions to linked list<object, start, end>
+		// tab will set the selection to the next one?
+
+		// how do we refresh to remove the thingy?
+
+		String toFieldText = toField.getText();
+		String ccFieldText = ccField.getText();
+		String subjectFieldText = subjectField.getText();
+		String bodyFieldText = bodyField.getText();
+
+		PlaceholderHelper ph = new PlaceholderHelper(toFieldText);
+		toField.select(ph.getStartPositionOfNextMatch(),
+				ph.getEndPositionOfNextMatch());
+
+		ph = new PlaceholderHelper(ccFieldText);
+		ccField.select(ph.getStartPositionOfNextMatch(),
+				ph.getEndPositionOfNextMatch());
+
+		ph = new PlaceholderHelper(subjectFieldText);
+		subjectField.select(ph.getStartPositionOfNextMatch(),
+				ph.getEndPositionOfNextMatch());
+
+		ph = new PlaceholderHelper(bodyFieldText);
+		bodyField.select(ph.getStartPositionOfNextMatch(),
+				ph.getEndPositionOfNextMatch());
+
 	}
 
 	/**
 	 * Default constructor of the Email Class.
 	 * <p>
-	 * Provides an Empty <code>JFrame</code> from which an email<br> 
+	 * Provides an Empty <code>JFrame</code> from which an email<br>
 	 * can be written.
 	 * <p>
 	 * Available fields are non empty<br>
 	 * <code>JTextField</code> with label <b>"To:"</b><br>
 	 * <code>JTextField</code> with label <b>"Cc:"</b><br>
-	 * <code>JTextField</code> with label <b>"Subject:" </b> with a maximum of 250 characters <br>
+	 * <code>JTextField</code> with label <b>"Subject:" </b> with a maximum of
+	 * 250 characters <br>
 	 * <code>JTextArea</code> with label <b>"Body"</b>
 	 */
 
@@ -172,25 +205,28 @@ public class EmailFrame extends JFrame {
 		// Button
 		setupToolBar();
 		// Entry Fields
-		setupEntryFields();		
+		setupEntryFields();
 	}
 
 	private void setupMenuBar() {
 
-		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		JMenu fileMenuBarEntry = new JMenu("File");
 		fileMenuBarEntry.setMnemonic('F');
 		menuBar.add(fileMenuBarEntry);
 
-		sendItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+		sendItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,
+				InputEvent.CTRL_DOWN_MASK));
 		fileMenuBarEntry.add(sendItem);
-		draftItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_DOWN_MASK));
+		draftItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D,
+				InputEvent.CTRL_DOWN_MASK));
 		fileMenuBarEntry.add(draftItem);
-		replyItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.CTRL_DOWN_MASK));
+		replyItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R,
+				InputEvent.CTRL_DOWN_MASK));
 		fileMenuBarEntry.add(replyItem);
-		forwardItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_DOWN_MASK));
+		forwardItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F,
+				InputEvent.CTRL_DOWN_MASK));
 		fileMenuBarEntry.add(forwardItem);
 		exitItem.setAccelerator(KeyStroke.getKeyStroke("ESCAPE"));
 
@@ -212,8 +248,6 @@ public class EmailFrame extends JFrame {
 				ExitMessage();
 			}
 		});
-		
-		
 
 	}
 
@@ -264,14 +298,14 @@ public class EmailFrame extends JFrame {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				saveTemplate();
 			}
-		});		
-		overwriteTemplate.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				overwriteTemplate();
-			}
-		});		
-		
-		
+		});
+		overwriteTemplate
+				.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent evt) {
+						overwriteTemplate();
+					}
+				});
+
 		add(bar, BorderLayout.NORTH);
 	}
 
@@ -288,8 +322,8 @@ public class EmailFrame extends JFrame {
 		mid.add(ccField);
 		mid.add(subject);
 		mid.add(subjectField);
-		add(mid, BorderLayout.LINE_START);				
-		
+		add(mid, BorderLayout.LINE_START);
+
 		bodyField.setLineWrap(true);
 		bodyField.setWrapStyleWord(true);
 		JScrollPane scroll = new JScrollPane(bodyField);
@@ -333,13 +367,13 @@ public class EmailFrame extends JFrame {
 		templateService.saveTemplate(templateView);
 		this.dispose();
 	}
-	
+
 	private void overwriteTemplate() {
 		setTemplateViewFields();
 		templateService.saveTemplate(templateView);
 		this.dispose();
 	}
-	
+
 	// Actions - Draft
 	/**
 	 * Draft email.
@@ -356,19 +390,20 @@ public class EmailFrame extends JFrame {
 	 * @return true, if successful
 	 */
 	private boolean validateEmailFields() {
-		if (!emailValidator.isValidSendees(emailView.getTo(), emailView.getCC())) {
-			JOptionPane.showMessageDialog(null, "One address is not properly formulated. Please recheck");
+		if (!emailValidator
+				.isValidSendees(emailView.getTo(), emailView.getCC())) {
+			JOptionPane.showMessageDialog(null,
+					"One address is not properly formulated. Please recheck");
 			return false;
 		}
 		return true;
 	}
 
-	
 	private void buildEmailViewObject() {
 		if (null == emailView.getId()) {
 			emailView.setId(id);
 		}
-		
+
 		emailView.setTo(toField.getText().trim());
 		emailView.setCC(ccField.getText().trim());
 		emailView.setSubject(subjectField.getText());
@@ -376,7 +411,8 @@ public class EmailFrame extends JFrame {
 	}
 
 	private void buildTemplateViewObject() {
-		String name = JOptionPane.showInputDialog(null, "Enter name of template");
+		String name = JOptionPane.showInputDialog(null,
+				"Enter name of template");
 		templateView.setName(name);
 		setTemplateViewFields();
 	}
@@ -387,8 +423,7 @@ public class EmailFrame extends JFrame {
 		templateView.setSubject(subjectField.getText());
 		templateView.setBody(bodyField.getText());
 	}
-	
-	
+
 	private void setMessageFields() {
 		this.id = emailView.getId();
 		this.toField.setText(emailView.getTo());
@@ -404,13 +439,10 @@ public class EmailFrame extends JFrame {
 		this.bodyField.setText(templateView.getBody());
 	}
 
-	
 	private void setExistingMessage() {
-		
+
 		subjectField.setDocument(new EntryFieldMaxLength(max_Length));
-		
-		
-		
+
 		// Draft Email
 		if (emailView.getFolder().equals(
 				CECConfigurator.getReference().get("Drafts"))) {
@@ -466,15 +498,15 @@ public class EmailFrame extends JFrame {
 		draft.setVisible(true);
 		saveTemplate.setVisible(false);
 		overwriteTemplate.setVisible(false);
-		
+
 		// OPTION AVAILABLE IN MENU
 		replyItem.setVisible(false);
 		forwardItem.setVisible(false);
 		sendItem.setVisible(true);
-		draftItem.setVisible(true);		
+		draftItem.setVisible(true);
 		saveTemplateItem.setVisible(false);
 	}
-	
+
 	private void setNewTemplate() {
 		subjectField.setDocument(new EntryFieldMaxLength(max_Length));
 		// OPTION AVAILABLE
@@ -484,15 +516,15 @@ public class EmailFrame extends JFrame {
 		draft.setVisible(false);
 		saveTemplate.setVisible(true);
 		overwriteTemplate.setVisible(false);
-		
+
 		// OPTION AVAILABLE IN MENU
 		replyItem.setVisible(false);
 		forwardItem.setVisible(false);
 		sendItem.setVisible(false);
-		draftItem.setVisible(false);		
-		saveTemplateItem.setVisible(true);		
+		draftItem.setVisible(false);
+		saveTemplateItem.setVisible(true);
 	}
-	
+
 	private void setOverwriteTemplate() {
 		subjectField.setDocument(new EntryFieldMaxLength(max_Length));
 		// OPTION AVAILABLE
@@ -502,16 +534,15 @@ public class EmailFrame extends JFrame {
 		draft.setVisible(false);
 		saveTemplate.setVisible(false);
 		overwriteTemplate.setVisible(true);
-		
+
 		// OPTION AVAILABLE IN MENU
 		replyItem.setVisible(false);
 		forwardItem.setVisible(false);
 		sendItem.setVisible(false);
-		draftItem.setVisible(false);		
-		saveTemplateItem.setVisible(false);		
+		draftItem.setVisible(false);
+		saveTemplateItem.setVisible(false);
 	}
-	
-	
+
 	/**
 	 * The only purpose of this private class<br>
 	 * is to provide in the JFRAME class <br>
@@ -520,24 +551,24 @@ public class EmailFrame extends JFrame {
 	 * <b>Source: </b><br>
 	 * "http://www.java-tips.org/java-se-tips/javax.swing/limit-jtextfield-input-to-a-maximum-length-3.html"
 	 */
-	
+
 	private class EntryFieldMaxLength extends PlainDocument {
 		private static final long serialVersionUID = 1L;
 		private int maxlength;
-		 
-		  EntryFieldMaxLength(int length) {
-		    //super();
-		    this.maxlength = length;
-		  }
 
-		  public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
-		    if (str == null)
-		      return;
-
-		    if ((getLength() + str.length()) <= maxlength) {
-		      super.insertString(offs, str, a);
-		    }
-		  }
+		EntryFieldMaxLength(int length) {
+			// super();
+			this.maxlength = length;
 		}
-}
 
+		public void insertString(int offs, String str, AttributeSet a)
+				throws BadLocationException {
+			if (str == null)
+				return;
+
+			if ((getLength() + str.length()) <= maxlength) {
+				super.insertString(offs, str, a);
+			}
+		}
+	}
+}
---
+++ CECProject/src/tests/PlaceholderHelperTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import cec.service.PlaceholderHelper;
+
+public class PlaceholderHelperTest {
+	
+	PlaceholderHelper phh;
+	String text;
+	Integer expectedStart;
+	Integer expectedEnd;
+
+	@Before
+	public void setUp() throws Exception {
+		text = "Hi ${friend} ${feeling}. ";
+		phh = new PlaceholderHelper(text);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void getIndexOfFirstMatch() {
+		expectedStart = 3;
+		expectedEnd = 12;
+		assertEquals(expectedStart, phh.getStartPositionOfNextMatch());
+		assertEquals(expectedEnd, phh.getEndPositionOfNextMatch());
+	}
+
+	@Test 
+	public void findNextIndeces() {
+		phh.findNext();
+		expectedStart = 13;
+		expectedEnd = 23;
+		assertEquals(expectedStart, phh.getStartPositionOfNextMatch());
+		assertEquals(expectedEnd, phh.getEndPositionOfNextMatch());		
+	}
+	
+	@Test
+	public void getPositionsOfAllPlaceHolders() {
+		Map<Integer,Integer> results = phh.positionsOfAllPlaceHolders();
+		
+		for(Map.Entry<Integer, Integer> entry: results.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+	}
+}
