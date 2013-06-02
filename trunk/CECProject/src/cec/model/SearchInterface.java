984de763562da21b25936a387bc6a3aae907acbb
deyvid.william@gmail.com <deyvid.william@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-02 00:30:13 +0000

    Search Interface

---
+++ trunk/CECProject/src/cec/model/SearchInterface.java
@@ -0,0 +1,18 @@
+package cec.model;
+
+/**
+ * The interface Search exposes the Search Engine methods 
+ *  
+ */
+
+public interface SearchInterface {
+  
+    /**
+     * Checks if the source text entry contains pattern to find
+     * 
+     *  @return true if matches
+     */
+	public boolean isMatch();
+
+}
+
