ff02cae512c15657b582b200f5d473d315faa72b
amish.gala@gmail.com <amish.gala@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-03 21:02:55 +0000

    Get the Rule tests running, and implement some more of the Rule

--- trunk/CECProject/src/cec/model/RuleSet.java
+++ trunk/CECProject/src/cec/model/RuleSet.java
@@ -1,8 +1,7 @@
 package cec.model;
 
 public interface RuleSet {
-	
 	public void swapRank(Rule first, Rule second);
 	public Iterable<Rule> loadRules();
-
+	public Iterable<Rule> loadActiveRules();
 }
--- trunk/CECProject/src/tests/RuleTests.java
+++ trunk/CECProject/src/tests/RuleTests.java
@@ -2,6 +2,8 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.UUID;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +16,7 @@ import cec.model.RuleImpl;
 
 public class RuleTests {
 	Rule ruleCUT;
+	EmailStub email;
 
 	@Before
 	public void setUp() throws Exception {
@@ -26,10 +29,142 @@ public class RuleTests {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void ruleAppliesToEmailAddressEmailMoved() {
+		email = new EmailStub();
+		email.from = "pankajkapania@yahoo.com";
+		ruleCUT.apply(email);
+		assertTrue(email.moveWasCalled);
 	}
 
+	@Test
+	public void ruleAppliesToWordsInSubjectEmailMoved() {
+		email = new EmailStub();
+		email.from = "billybob@gmail.com"; //not found
+		email.subject = "These are some jokes for you"; // the word jokes should be found
+		ruleCUT.apply(email);
+		assertTrue(email.moveWasCalled);
+	}
+
+	@Test
+	public void ruleAppliesToWordsInBodyEmailMoved() {
+		email = new EmailStub();
+		email.from = "billybob@gmail.com"; //not found
+		email.subject = "bad man";
+		email.body = "These are some jokes for you"; // the word jokes should be found
+		ruleCUT.apply(email);
+		assertTrue(email.moveWasCalled);
+	}
+	
+	@Test
+	public void ruleDoesNotApply() {
+		email = new EmailStub();
+		email.from = "billy@gmail.com";
+		email.subject = "no joke";
+		email.body = "i like coen 491";
+		ruleCUT.apply(email);
+		assertFalse(email.moveWasCalled);
+	}
+
+}
+
+class EmailStub implements Email {
+	public String from;
+	public String subject;
+	public String body;
+	public boolean moveWasCalled;
+
+	@Override
+	public int compareTo(Email o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isMatch(String pattern) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void send() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveToDraftFolder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move(Folder destDir) {
+		moveWasCalled = true;
+	}
+
+	@Override
+	public UUID getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFrom() {
+		return from;	
+	}
+
+	@Override
+	public String getTo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCC() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSubject() {
+		return subject;
+	}
+
+	@Override
+	public String getBody() {
+		return body;
+	}
+
+	@Override
+	public String getSentTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLastModifiedTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Folder getParentFolder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLastModifiedTimeNicelyFormatted() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
 
 class FolderStub extends Folder {
