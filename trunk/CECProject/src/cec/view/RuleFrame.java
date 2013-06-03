2bc455c7f2184b1bac8ced3bb9f67b1167bec885
deyvid.william@gmail.com <deyvid.william@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-03 22:10:36 +0000

    Rule Frame UI changes

--- trunk/CECProject/src/cec/view/RuleFrame.java
+++ trunk/CECProject/src/cec/view/RuleFrame.java
@@ -1,11 +1,16 @@
 package cec.view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JSeparator;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
 
 public class RuleFrame extends JFrame {
@@ -20,7 +25,7 @@ public class RuleFrame extends JFrame {
 	
 	
 	private void initialize() {		
-		setSize(410, 350);
+		setSize(460, 380);
 		setLayout(new BorderLayout());
 		setResizable(false);
 		setVisible(true);
@@ -31,7 +36,7 @@ public class RuleFrame extends JFrame {
 		// Button
 		setupToolBar();
 		// Entry Fields
-		//setupEntryFields();
+		setupEntryFields();
 	}
 	
 	private void setupToolBar() {
@@ -46,4 +51,40 @@ public class RuleFrame extends JFrame {
 		add(bar, BorderLayout.NORTH);
 		add(hBar);
 	}
+	
+	private void setupEntryFields() {
+		
+		JPanel botPanel = new JPanel(new BorderLayout());
+		JPanel botTop = new JPanel();
+		JPanel botMid = new JPanel();
+		JPanel botBot = new JPanel();
+		
+		JLabel when = new JLabel("When receiving Emails");
+		botTop.add(when);
+		
+		JLabel from = new JLabel("From:         ");
+		JTextField fromField = new JTextField("", 22);
+		JLabel content = new JLabel("Containing whe words: ");
+		JTextField contentField = new JTextField("", 22);
+		botMid.add(from);
+		botMid.add(fromField);
+		botMid.add(content);
+		botMid.add(contentField);
+		
+		JLabel move = new JLabel("Move to: ");
+		JTextField moveField = new JTextField("", 22);
+		botBot.add(move);
+		botBot.add(moveField);	
+
+	//	midTop.setPreferredSize(new Dimension(50, 50));
+
+
+		
+		botPanel.add(botTop, BorderLayout.NORTH);
+		botPanel.add(botMid, BorderLayout.EAST);
+		botPanel.add(botBot, BorderLayout.SOUTH);
+		add(botPanel, BorderLayout.SOUTH);
+		
+	}	
+	
 }
