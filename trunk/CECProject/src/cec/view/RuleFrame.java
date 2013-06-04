eaefc23b0bbfcfcd3e141f32d7515323b82479f8
romeo.honvo <romeo.honvo@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-04 03:23:10 +0000

    Rule Frame

--- trunk/CECProject/src/cec/view/RuleFrame.java
+++ trunk/CECProject/src/cec/view/RuleFrame.java
@@ -1,17 +1,22 @@
 package cec.view;
 
 import java.awt.BorderLayout;
+import java.awt.Choice;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
+
+import cec.service.FolderService;
 
 public class RuleFrame extends JFrame {
 	
@@ -25,7 +30,8 @@ public class RuleFrame extends JFrame {
 	
 	
 	private void initialize() {		
-		setSize(460, 380);
+		setSize(460, 360);
+		//setSize(610, 470);
 		setLayout(new BorderLayout());
 		setResizable(false);
 		setVisible(true);
@@ -41,6 +47,7 @@ public class RuleFrame extends JFrame {
 	
 	private void setupToolBar() {
 		JToolBar bar = new JToolBar();
+		bar.setPreferredSize(new Dimension(460, 60));
 		ImageIcon sendIcon = new ImageIcon("images/email_send.png");
 		JButton send = new JButton(" Save >>   ");
 		JSeparator hBar = new JSeparator();
@@ -54,36 +61,66 @@ public class RuleFrame extends JFrame {
 	
 	private void setupEntryFields() {
 		
+		//JPanel botPanel = new JPanel();
 		JPanel botPanel = new JPanel(new BorderLayout());
+		//botPanel.setLayout(new GridLayout(3,1));
+		botPanel.setPreferredSize(new Dimension(460, 270));
+
+		
+		
 		JPanel botTop = new JPanel();
+		botTop.setPreferredSize(new Dimension(460, 90));
+		
 		JPanel botMid = new JPanel();
+		botMid.setPreferredSize(new Dimension(460, 90));
+		
 		JPanel botBot = new JPanel();
+		botBot.setPreferredSize(new Dimension(460, 90));
 		
-		JLabel when = new JLabel("When receiving Emails");
-		botTop.add(when);
+		//JLabel when = new JLabel("When receiving Emails");
+		//botTop.add(when);		
 		
-		JLabel from = new JLabel("From:         ");
-		JTextField fromField = new JTextField("", 22);
-		JLabel content = new JLabel("Containing whe words: ");
-		JTextField contentField = new JTextField("", 22);
-		botMid.add(from);
-		botMid.add(fromField);
-		botMid.add(content);
-		botMid.add(contentField);
+		JLabel from = new JLabel("From:        ");
+		JTextField fromField = new JTextField("", 45);
+		botTop.add(from,BorderLayout.WEST);
+		botTop.add(fromField,BorderLayout.EAST);
+
 		
-		JLabel move = new JLabel("Move to: ");
-		JTextField moveField = new JTextField("", 22);
-		botBot.add(move);
-		botBot.add(moveField);	
-
-	//	midTop.setPreferredSize(new Dimension(50, 50));
-
-
+		JLabel content = new JLabel("Containing:");
+		JTextField contentField = new JTextField("", 45);
+			
+		botMid.add(content,BorderLayout.WEST);
+		botMid.add(contentField,BorderLayout.EAST);
+		
+	
+		
+		
+		
+		JLabel move = new JLabel("Move to:    ");
+		//JTextField moveField = new JTextField("", 45);
+		Choice folderChoosed=new Choice();
+		folderChoosed.setPreferredSize(new Dimension(350, 00));
+		/***/
+		FolderService folderService = new FolderService();
+		ArrayList<String> listOfFolders = (ArrayList<String>) folderService.loadHierarchy();
+		
+		for (String folder : listOfFolders)
+			folderChoosed.add(folder);
+		/***/
+		
+		
+		
+		botBot.add(move,BorderLayout.WEST);
+		botBot.add(folderChoosed,BorderLayout.EAST);
+		
+		
 		
 		botPanel.add(botTop, BorderLayout.NORTH);
-		botPanel.add(botMid, BorderLayout.EAST);
+		botPanel.add(botMid, BorderLayout.CENTER);
 		botPanel.add(botBot, BorderLayout.SOUTH);
 		add(botPanel, BorderLayout.SOUTH);
+		
+		
 		
 	}	
 	
