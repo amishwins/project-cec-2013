463e921f7a9feb59e6c9ebc678dcb8ff11f209e2
deyvid.william@gmail.com <deyvid.william@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-05 01:14:07 +0000

    Rule Settings Frame and Actions

---
+++ trunk/CECProject/src/cec/view/RuleListViewData.java
@@ -0,0 +1,70 @@
+package cec.view;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+/** CREATE!!
+ * Auxiliary Class used by <code>EmailClient</code> class to fill the JTable
+ *  <code>emailTable</code> that shows the Emails of the selected folder.
+ * It receives trough its constructor a String containing the columns
+ * and list of Email Entities and returns the data in a Model that is
+ * compatible with the JTable component. To do this, this class extends
+ * the AbstractTableModel class and it's default implementations.
+ * */
+
+public class RuleListViewData extends AbstractTableModel {
+	
+	private static final long serialVersionUID = 5641320475551936954L;
+	String[] header;
+	ArrayList<RuleViewEntity> data;
+	
+	public RuleListViewData(String[] ruleTableViewColumns,  Iterable<RuleViewEntity> ruleTableViewData) { 
+		header = ruleTableViewColumns;
+		data   = (ArrayList<RuleViewEntity>)ruleTableViewData;
+		
+	}
+	
+	public RuleViewEntity getViewEntityAtIndex(int index) {
+		
+		if (index < 0)
+			return new RuleViewEntity();
+		
+		return data.get(index);
+	}
+	
+	public int getRowCount() {
+		return data.size();
+	}
+	
+    public int getColumnCount() { 
+    	return 4; 
+	}
+    
+    public Object getValueAt(int row, int column){ 
+    	//RuleViewEntity currentRow = data.get(row);
+    	String columnValue = "";
+    	switch (column) {
+			case 0:
+				columnValue = "a";//currentRow.getEmailAddresses();		
+				break;
+			case 1:
+				columnValue = "b";//currentRow.getWords();
+				break;
+			case 2:
+				columnValue = "c";//currentRow.getFolderPath();
+				break;
+			case 3:
+				columnValue = "d";// currentRow.getFolderPath();
+				break;				
+			default:
+				break;
+		}   
+    	
+    	return columnValue;
+	}        
+    
+    public String getColumnName(int i) {
+    	return header[i];
+    }
+}
--- trunk/CECProject/src/cec/view/RuleSettings.java
+++ trunk/CECProject/src/cec/view/RuleSettings.java
@@ -1,166 +1,64 @@
-/*package cec.view;
-
-import java.awt.BorderLayout;
-import java.awt.Choice;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.util.ArrayList;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JSeparator;
-import javax.swing.JTextField;
-import javax.swing.JToolBar;
-
-import cec.service.FolderService;
-
-public class RuleFrame extends JFrame {
-	
-	public RuleFrame(){
-		initialize();
-	}
-	
-	public RuleFrame(String rule){
-		initialize();
-	}
-	
-	
-	private void initialize() {		
-		setSize(460, 360);
-		//setSize(610, 470);
-		setLayout(new BorderLayout());
-		setResizable(false);
-		setVisible(true);
-		setLocationRelativeTo(null);
-
-		// Menu
-		//setupMenuBar();
-		// Button
-		setupToolBar();
-		// Entry Fields
-		setupEntryFields();
-	}
-	
-	private void setupToolBar() {
-		JToolBar bar = new JToolBar();
-		bar.setPreferredSize(new Dimension(460, 60));
-		ImageIcon sendIcon = new ImageIcon("images/email_send.png");
-		JButton send = new JButton(" Save >>   ");
-		JSeparator hBar = new JSeparator();
-		
-		send.setIcon(sendIcon);
-		bar.setFloatable(false);
-		bar.add(send);		
-		add(bar, BorderLayout.NORTH);
-		add(hBar);
-	}
-	
-	private void setupEntryFields() {
-		
-		//JPanel botPanel = new JPanel();
-		JPanel botPanel = new JPanel(new BorderLayout());
-		//botPanel.setLayout(new GridLayout(3,1));
-		botPanel.setPreferredSize(new Dimension(460, 270));
-
-		
-		
-		JPanel botTop = new JPanel();
-		botTop.setPreferredSize(new Dimension(460, 90));
-		
-		JPanel botMid = new JPanel();
-		botMid.setPreferredSize(new Dimension(460, 90));
-		
-		JPanel botBot = new JPanel();
-		botBot.setPreferredSize(new Dimension(460, 90));
-		
-		//JLabel when = new JLabel("When receiving Emails");
-		//botTop.add(when);		
-		
-		JLabel from = new JLabel("From:        ");
-		JTextField fromField = new JTextField("", 45);
-		botTop.add(from,BorderLayout.WEST);
-		botTop.add(fromField,BorderLayout.EAST);
-
-		
-		JLabel content = new JLabel("Containing:");
-		JTextField contentField = new JTextField("", 45);
-			
-		botMid.add(content,BorderLayout.WEST);
-		botMid.add(contentField,BorderLayout.EAST);
-		
-	
-		
-		
-		
-		JLabel move = new JLabel("Move to:    ");
-		//JTextField moveField = new JTextField("", 45);
-		Choice folderChoosed=new Choice();
-		folderChoosed.setPreferredSize(new Dimension(350, 00));
-		
-		FolderService folderService = new FolderService();
-		ArrayList<String> listOfFolders = (ArrayList<String>) folderService.loadHierarchy();
-		
-		for (String folder : listOfFolders)
-			folderChoosed.add(folder);
-		
-		
-		
-		
-		botBot.add(move,BorderLayout.WEST);
-		botBot.add(folderChoosed,BorderLayout.EAST);
-		
-		
-		
-		botPanel.add(botTop, BorderLayout.NORTH);
-		botPanel.add(botMid, BorderLayout.CENTER);
-		botPanel.add(botBot, BorderLayout.SOUTH);
-		add(botPanel, BorderLayout.SOUTH);
-		
-		
-		
-	}	
-	
-}*/
 package cec.view;
 
 import java.awt.BorderLayout;
-import java.awt.Choice;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
-import javax.swing.JTextField;
+import javax.swing.JTable;
 import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
-import cec.config.CECConfigurator;
 import cec.service.FolderService;
 
 public class RuleSettings extends JFrame {
 	
-	JCheckBox from = new JCheckBox("From:        ");
-	JCheckBox content = new JCheckBox("Containing:");
-	JTextField fromField = new JTextField("", 40);
-	JTextField contentField = new JTextField("", 40);
-	Choice folderChoosed= new Choice();
+	
+	
+	Object rowData[][] = { { "Row1-Column1", "Row1-Column2", "Row1-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            { "Row2-Column1", "Row2-Column2", "Row2-Column3"},
+            
+	
+	
+	
+	};
+Object columnNames[] = { "Column One", "Column Two", "Column Three"};
+JTable ruleTable = new JTable(rowData, columnNames);
+FolderService folderService = new FolderService();
+
+EmailViewEntity selectedRuleEntity;
+
+
 	
 	public RuleSettings(){
 		initialize();
@@ -171,7 +69,7 @@ public class RuleSettings extends JFrame {
 	}	
 	
 	private void initialize() {		
-		setSize(460, 278);
+		setSize(515, 370);
 		setLayout(new BorderLayout());
 		setResizable(false);
 		setVisible(true);
@@ -180,10 +78,12 @@ public class RuleSettings extends JFrame {
 		setupMenuBar();
 		setupToolBar();
 		setupRuleTable();
+		loadRuleTable();
 	}
 
 	private void setupMenuBar() {
-		JMenuItem saveItem = new JMenuItem("Save", KeyEvent.VK_S);
+		JMenuItem editItem = new JMenuItem("Edit Rule", KeyEvent.VK_D);
+		JMenuItem deleteItem = new JMenuItem("Delete Rule", KeyEvent.VK_E);
 		JMenuItem exitItem = new JMenuItem("Exit");
 		
 		JMenuBar menuBar = new JMenuBar();
@@ -191,22 +91,35 @@ public class RuleSettings extends JFrame {
 		JMenu fileMenuBarEntry = new JMenu("File");
 		fileMenuBarEntry.setMnemonic('F');
 		menuBar.add(fileMenuBarEntry);
-
-		saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
-		fileMenuBarEntry.add(saveItem);
-
+		JMenu editMenuBarEntry = new JMenu("Edit");
+		editMenuBarEntry.setMnemonic('E');
+		menuBar.add(editMenuBarEntry);	
+		
 		exitItem.setAccelerator(KeyStroke.getKeyStroke("ESCAPE"));
 		fileMenuBarEntry.add(exitItem);
+
+		editItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_DOWN_MASK));
+		editMenuBarEntry.add(editItem);
+		editMenuBarEntry.addSeparator();
+		deleteItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_DOWN_MASK));
+		editMenuBarEntry.add(deleteItem);			
+
 			
-		saveItem.addActionListener(new java.awt.event.ActionListener() {
+		editItem.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
-			//	saveRule();
+				menuEditEditRule();
+			}
+		});
+		
+		deleteItem.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				menuEditDeleteRule();
 			}
 		});
 		
 		exitItem.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
-			//	exitRule();
+				exitRuleSett();
 			}
 		});		
 	}
@@ -216,19 +129,22 @@ public class RuleSettings extends JFrame {
 		JToolBar bar = new JToolBar();
 		bar.setPreferredSize(new Dimension(460, 60));
 		ImageIcon applyIcon = new ImageIcon("images/rule_save.png");
+		ImageIcon updateIcon = new ImageIcon("images/rule_edit.png");		
 		ImageIcon deleteIcon = new ImageIcon("images/rule_delete.png");
 		JButton apply = new JButton(" Apply");
 		JButton applyAll = new JButton(" Apply All >>");
+		JButton edit = new JButton("Edit");
 		JButton delete = new JButton("Delete");
-		JSeparator hBar = new JSeparator();
 		
 		apply.setIcon(applyIcon);
 		applyAll.setIcon(applyIcon);
+		edit.setIcon(updateIcon);
 		delete.setIcon(deleteIcon);
 		
 		bar.setFloatable(false);
 		bar.add(apply);
 		bar.add(applyAll);
+		bar.add(edit);		
 		bar.add(delete);
 		
 		add(bar, BorderLayout.NORTH);		
@@ -245,6 +161,11 @@ public class RuleSettings extends JFrame {
 			}
 		});	
 
+		edit.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				menuEditEditRule();
+			}
+		});		
 		delete.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				//deleteRule();
@@ -253,105 +174,101 @@ public class RuleSettings extends JFrame {
 	}
 	
 	private void setupRuleTable() {
-		
+
 		JPanel midPanel = new JPanel(new BorderLayout());
 		JPanel botPanel = new JPanel(new BorderLayout());
+		JPanel botRight = new JPanel();
+		JScrollPane botLeft = new JScrollPane(ruleTable,
+				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);		
 		
-		JPanel botTop = new JPanel();
-		JPanel botMid = new JPanel();
-		JPanel botBot = new JPanel();
 		
-		botTop.setPreferredSize(new Dimension(460, 50));	
-		botMid.setPreferredSize(new Dimension(460, 50));		
-		botBot.setPreferredSize(new Dimension(460, 50));
+		JButton moveTop = new JButton("Top ");
+		JButton moveDown = new JButton("Down");
 		
+		botLeft.setPreferredSize(new Dimension(430, 250));	
+		botRight.setPreferredSize(new Dimension(55, 50));		
+				
 		JSeparator hBar = new JSeparator();
-		JLabel when = new JLabel("  When receiving Emails");
 		midPanel.add(hBar, BorderLayout.NORTH);
-		midPanel.add(when, BorderLayout.SOUTH);		
-		
-		botTop.add(from,BorderLayout.WEST);
-		botTop.add(fromField,BorderLayout.EAST);		
-
-		botMid.add(content,BorderLayout.WEST);
-		botMid.add(contentField,BorderLayout.EAST);		
-				
-		JLabel move = new JLabel("        Move to:    ");
-
-		//Choice folderChoosed=new Choice();
-		folderChoosed.setPreferredSize(new Dimension(330, 00));
-		/***/
-		FolderService folderService = new FolderService();
-		ArrayList<String> listOfFolders = (ArrayList<String>) folderService.loadHierarchy();
-		/*ArrayList<String> listOfFolders = (ArrayList<String>) folderService.loadHierarchy(
-				CECConfigurator.getReference().get("Inbox"));*/
-		
-		for (String folder : listOfFolders)
-			folderChoosed.add(folder);
-		
-		
-		/***/
-				
-		botBot.add(move,BorderLayout.WEST);
-		botBot.add(folderChoosed,BorderLayout.EAST);
-		
-		
 		add(midPanel);
 		
-		botPanel.add(botTop, BorderLayout.NORTH);
-		botPanel.add(botMid, BorderLayout.CENTER);
-		botPanel.add(botBot, BorderLayout.SOUTH);
-		add(botPanel, BorderLayout.SOUTH);
-		
-		from.setSelected(true);	
-		from.addItemListener(new ruleSelection());
-		content.setSelected(true);
-		content.addItemListener(new ruleSelection());		
-		
-	}
-	
-	//Actions > Save Rule
-	private void saveRule(){
-		
-		if ((!from.isSelected()) &&(!content.isSelected())){
-			JOptionPane.showMessageDialog(null, "Select at least one of the methods of implementation");
-		}
-		else
-		{
-			
-			// Building the Rules 
-			String choosed =folderChoosed.getSelectedItem();
-			JOptionPane.showMessageDialog(null, "From "+fromField.getText()+"\nContaining "+contentField.getText()
-													+"\n Selected Folder = "+choosed);							
-		}		
-	}
-	
-	//Actions > Exit
-	private void exitRule(){
-		this.dispose();		
-	}
-	
-	private class ruleSelection implements ItemListener {	
-		public void itemStateChanged(ItemEvent e) {
-			Object source = e.getItemSelectable();
+		botRight.add(moveTop,BorderLayout.NORTH);		
+		botRight.add(moveDown,BorderLayout.SOUTH);		
+		botPanel.add(botLeft, BorderLayout.WEST);
+		botPanel.add(botRight, BorderLayout.CENTER);
 
-			if (source == from) {
-				if (e.getStateChange() == ItemEvent.DESELECTED){
-					fromField.setText("");
-					fromField.setEnabled(false);
-				}
-				else
-					fromField.setEnabled(true);	
-			}	    	
-			else if (source == content) {
-				if (e.getStateChange() == ItemEvent.DESELECTED){
-					contentField.setText("");
-					contentField.setEnabled(false);
-				}
-				else
-					contentField.setEnabled(true);	
-			}   
+		add(botPanel, BorderLayout.SOUTH);	
+		
+		ruleTable.setFillsViewportHeight(true);
+		ruleTable.getSelectionModel().addListSelectionListener(new ruleTableRowListener());
+		ruleTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+	}
+	
+	private void defineRuleTableLayout() {
+		ruleTable.getColumnModel().getColumn(0).setPreferredWidth(70);		
+		ruleTable.getColumnModel().getColumn(1).setPreferredWidth(70);
+		ruleTable.getColumnModel().getColumn(2).setPreferredWidth(70);
+		ruleTable.getColumnModel().getColumn(2).setPreferredWidth(30);	
+	}	
+	
+	
+	public void loadRuleTable() {
+		String[] ruleTableViewColumns = { "From", "Words", "Folder","Rank" };
+		Iterable<EmailViewEntity> emailsInEachFolder = folderService.loadEmails("emails\\Inbox");
+		ruleTable.setModel(new EmailListViewData(ruleTableViewColumns, emailsInEachFolder));	
+		defineRuleTableLayout();
+	}	
+	
+	private void setSelectedRuleEntity(EmailViewEntity emailViewEntity) {
+		selectedRuleEntity = emailViewEntity;
+	}
+	
+	private EmailViewEntity getSelectedRuleEntity() {
+		return selectedRuleEntity; 
+	}
+	
+		
+	// RULE TABLE MAIN LISTENER
+	private class ruleTableRowListener implements ListSelectionListener {
+		public void valueChanged(ListSelectionEvent event) {
+			if (event.getValueIsAdjusting()) {
+				return;
+			} else {
+				setSelectedRuleEntity(((EmailListViewData) (ruleTable
+						.getModel())).getViewEntityAtIndex(ruleTable
+						.getSelectedRow()));
+			}
 		}
+	}
+
+	// Actions > Update Rule
+	private void menuEditEditRule() {			
+			if (getSelectedRuleEntity() == null) {
+				JOptionPane.showMessageDialog(null, "Select a rule to update");
+			} else {
+				new RuleFrame();
+				// new RuleFrame(selectedRuleEntity);
+
+			}
+		}
+	
+	// Actions > Delete Rule
+	private void menuEditDeleteRule() {			
+			if (getSelectedRuleEntity() == null) {
+				JOptionPane.showMessageDialog(null, "Select a rule to delete");
+			} else {
+				//new RuleFrame();
+				// new RuleFrame(selectedRuleEntity);
+			}
+		}	
+	
+	
+
+	// Actions > Exit
+	private void exitRuleSett() {
+		this.dispose();
 	}
 	
 }
