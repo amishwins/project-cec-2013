2042b8a7bb45682b6273aa175a12ef2c4fe61dea
pankajkapania <pankajkapania@gmail.com>
2013-06-06 21:34:47 +0000

    Added JavaDocs for the Service layer: RuleService class.

--- CECProject/src/cec/service/RuleService.java
+++ CECProject/src/cec/service/RuleService.java
@@ -14,9 +14,6 @@ import cec.model.RuleSetFactory;
 import cec.view.RuleViewEntity;
 
 /**
- * PLEASE COMPLETE
- * 
- * 
  * Represents the actions that can be performed against a Rule from
  * the Presentation Layer (cec.view) such as creating, updating, deleting, 
  * shuffling, apply and apply all; also retrieves information 
@@ -25,12 +22,25 @@ import cec.view.RuleViewEntity;
  */
 
 public class RuleService {
+	
+	/** The rule set. */
 	private RuleSet ruleSet;	
 	
+	/**
+	 * Instantiates a new rule service.
+	 */
 	public RuleService() {
 		ruleSet = RuleSetFactory.getRuleSetInstance();
 	}
 
+	
+	
+	/**
+	 * It saves the rule in the system.
+	 * Postcondition: Rule has been saved into the system. 
+	 *
+	 * @param ruleViewEntity the rule view entity
+	 */
 	public void save(RuleViewEntity ruleViewEntity) {
 		Rule rule = (new RuleBuilder())
 				.withId(UUID.randomUUID())
@@ -43,6 +53,13 @@ public class RuleService {
 		rule.save();  // should this call the ruleset?
 	}	
 	
+	/**
+	 * Precondition: rule exists in the system.
+	 * PostCondition: rule does not exist in the system.
+	 * this method deletes the rule from the system.
+	 *
+	 * @param ruleViewEntity the rule view entity
+	 */
 	public void delete(RuleViewEntity ruleViewEntity) {
 		Rule rule = (new RuleBuilder())
 				.withId(ruleViewEntity.getID())
@@ -55,6 +72,12 @@ public class RuleService {
 		rule.delete();
 	}
 
+	/**
+	 * Precondition: rule exists in the system.
+	 * This method Load all rules from the system.
+	 *
+	 * @return the iterable
+	 */
 	public Iterable<RuleViewEntity> loadAllRules() {
 		List<RuleViewEntity> listOfRuleViewEntities = new ArrayList<>();
 		Iterable<Rule> rules = ruleSet.loadRules();
@@ -70,6 +93,12 @@ public class RuleService {
 		return listOfRuleViewEntities;
 	}
 
+	/**
+	 * it builds the rule from the RuleViewEntity and then applies to 
+	 * email or collection of email(s).
+	 * 
+	 * @param ruleViewEntity the rule view entity
+	 */
 	public void apply(RuleViewEntity ruleViewEntity) {
 		Rule rule = (new RuleBuilder())
 				.withId(ruleViewEntity.getID())
@@ -84,12 +113,30 @@ public class RuleService {
 		
 	}
 
+	/**
+	 * Postcondition: all the emails will be moved to the new folder specified 
+	 * by the rule after the invocation of this rule on email(s) present in the 
+	 * inbox folder.
+	 * it builds the rule from the RuleViewEntity and then applies to 
+	 * email or collection of email(s).
+	 * 
+	 * @param ruleViewEntity the rule view entity
+	 */
 	public void applyAll() {
 		RuleSet ruleSet = RuleSetFactory.getRuleSetInstance();
 		ruleSet.apply(FolderFactory.getFolder(CECConfigurator.getReference().get("Inbox")).loadEmails());
 		
 	}
 
+	/**
+	 * Precondition: Rule exist in the system and new values have been 
+	 * specified by the user.
+	 *  
+	 * It builds the rule from the RuleViewEntity and then updates the new data values
+	 * for the existing rule.
+	 *
+	 * @param ruleViewEntity the rule view entity
+	 */
 	public void update(RuleViewEntity ruleViewEntity) {
 		Rule rule = (new RuleBuilder())
 				.withId(ruleViewEntity.getID())
@@ -104,6 +151,16 @@ public class RuleService {
 		
 	}
 
+	/**Precondition: Rule specified in the argument previous and current exist with the different 
+	 * id and rank exist in the system.
+	 * Postcondition: rank of the rules have been swapped.
+	 * Invariant: Rule id does not change during the shuffle process.
+	 * 
+	 * It swaps the ranks of the rules.
+	 *
+	 * @param previous the previous
+	 * @param current the current
+	 */
 	public void shuffle(RuleViewEntity previous, RuleViewEntity current) {
 		Rule previousRule = (new RuleBuilder())
 				.withId(previous.getID())
