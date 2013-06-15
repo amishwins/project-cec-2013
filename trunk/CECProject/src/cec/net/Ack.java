125f52819fd7bdb8ab6504ddd805dedb51cd6340
pankajkapania@gmail.com <pankajkapania@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-15 00:26:10 +0000

    Ack is now serializable!

--- trunk/CECProject/src/cec/net/Ack.java
+++ trunk/CECProject/src/cec/net/Ack.java
@@ -1,8 +1,13 @@
 package cec.net;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public class Ack {
+public class Ack implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private UUID id;
 	private MessageType msgType;
 	
