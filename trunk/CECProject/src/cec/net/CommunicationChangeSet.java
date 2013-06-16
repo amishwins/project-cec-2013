ca91146c911c8d79ce6ac48f80c48a5cd43d2e4b
amish.gala@gmail.com <amish.gala@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-16 02:47:02 +0000

    Make this class serializable. What a mitsake!!! :)

--- trunk/CECProject/src/cec/net/CommunicationChangeSet.java
+++ trunk/CECProject/src/cec/net/CommunicationChangeSet.java
@@ -1,9 +1,11 @@
 package cec.net;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public class CommunicationChangeSet {
+public class CommunicationChangeSet implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	private final ChangeSetState state; 
 	private final UUID meetingID;
 	
