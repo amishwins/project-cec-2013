9bf9199f6070a75557d690c0534b9d0e20a1b7a2
deyvid.william@gmail.com <deyvid.william@gmail.com@3dd7631b-4698-b549-0264-e2e999cf1980>
2013-06-19 00:12:11 +0000

    Javadocs - Network Layer

--- trunk/CECProject/src/cec/net/HandShake.java
+++ trunk/CECProject/src/cec/net/HandShake.java
@@ -2,6 +2,11 @@ package cec.net;
 
 import java.io.Serializable;
 
+/**
+ * Serializable Object that represents a handshake beetween Client and Server 
+ * that identifies the Client email adress and is used to set up the Server Mapping [email/Socket].
+ */
+
 public class HandShake implements Serializable {
 	private static final long serialVersionUID = 1L;
 	public String emailAddress;
